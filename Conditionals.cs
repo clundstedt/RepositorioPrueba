c36fb003760656cb3e88fac5920cfda908016b65
Christian Lundstedt <clundstedt@innovasoft.cl>
2019-03-14 17:25:14 -0300

    Cambia N° de Ejercicio y Write por ReadLine

--- Conditionals.cs
+++ Conditionals.cs
@@ -9,13 +9,13 @@ namespace CSharp1Exercises.FlujoDeControl
         /// a valid number, display "Valid" on the console. Otherwise, display "Invalid". (This logic is used a lot in 
         /// applications where values entered into input boxes need to be validated.)
         /// </summary>
-        public void Exercise1()
+        public void Exercise14()
         {
             Console.Write("Enter a number between 1 to 10: ");
             var input = Console.ReadLine();
             var number = Convert.ToInt32(input);
             if (number >= 1 && number <= 10)
-                Console.WriteLine("Valid");
+                Console.ReadLine("Valid");
             else
                 Console.WriteLine("Invalid");
         }
